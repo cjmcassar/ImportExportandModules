0dd21deebcdffb87dbbffc246d9d5b896ceecf85
Christopher-John Cassar <46441540+cjmcassar@users.noreply.github.com>
2021-01-02 00:16:45 +0000

    created math.js file and imported it into index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi, { DoublePi, TriplePi } from "./math";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi}</li>
+    <li>{DoublePi()}</li>
+    <li>{TriplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
---
+++ src/math.js
@@ -0,0 +1,12 @@
+const pi = 3.145962;
+
+function DoublePi() {
+  return pi * 2;
+}
+
+function TriplePi() {
+  return pi * 3;
+}
+
+export default pi;
+export { DoublePi, TriplePi };
